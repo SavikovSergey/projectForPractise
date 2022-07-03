b6dc98c8488895cb1d68fb42e56b44e7fd926c65
Savikov Sergey <mega.savik25@mail.ru>
2022-07-03 20:44:09 +0300

    Строки

---
+++ strings.py
@@ -0,0 +1,44 @@
+string1 = 'String'
+string2 = "String"
+print(string1,string2)
+
+
+string = "First line of text. " \
+    "Second line of text"
+print(string)
+
+print()
+
+string = "First line of text. \
+    Second line of text"
+print(string)
+
+print()
+
+string = "First line of text.\n " \
+    "Second line of text"
+print(string)
+
+multiline_string = """
+Lesson1. Variables and Data Types
+Some data types explained in this lesson:
+- int
+- bool
+- float
+- complex
+- str
+"""
+print(multiline_string)
+
+print()
+
+multiline_string = '''
+Lesson1. Variables and Data Types
+Some data types explained in this lesson:
+- int
+- bool
+- float
+- complex
+- str
+'''
+print(multiline_string)
