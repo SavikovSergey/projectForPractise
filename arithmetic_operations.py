b885e019a284580eebb0e94e50d3cbce1ab601ff
Savikov Sergey <mega.savik25@mail.ru>
2022-07-03 19:37:56 +0300

    Арифметические операции

---
+++ arithmetic_operations.py
@@ -0,0 +1,46 @@
+x = 2
+y = 8
+
+result = x+y
+print(result)
+print(x+y)
+print(x+3)
+print(x-y)
+
+print()
+
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+
+print()
+
+print(x ** y)
+print(x ** 0.5)
+print(pow(2,y))
+
+print()
+
+z = -2
+print(abs(z))
+print(z ** 2)
+
+print()
+
+z = 3
+print(abs(z))
+
+print()
+
+print(4.8*0.6+2.28/3-5.1 ** 2)
+
+print()
+
+number = 3.22
+print(round(number),round(number,1))
+
+print()
+
+number = 3.88
+print(round(number),round(number,1))
