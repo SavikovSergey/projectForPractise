226d330adba9800119b1805e9c618085c88bd73c
Savikov Sergey <mega.savik25@mail.ru>
2022-07-04 19:46:29 +0300

    truth_value(Lesson 3)

---
+++ truth_value_tested.py
@@ -0,0 +1,31 @@
+string = input('Enter a string: ')
+if string != '':
+    print('The string is "{}"'.format(string))
+
+    print()
+
+string = input('Enter a string: ')
+if string:
+        print('The string is "{}"'.format(string))
+
+        print()
+
+number = int(input('Enter a number: '))
+if number:
+    print(number)
+
+print()
+
+number = int(input('Enter an integer: '))
+if number != 0:
+    print('The number is not zero')
+else:
+    print('The number is zero')
+
+print()
+
+number = int(input('Enter an integer: '))
+if number:
+    print('The number is not zero')
+else:
+    print('The number is zero')
