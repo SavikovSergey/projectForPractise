b3d24cc7afe829c03eeb394dba81dc316af98ccd
Savikov Sergey <mega.savik25@mail.ru>
2022-07-04 17:11:06 +0300

    elif_switch(Lesson 3)

---
+++ elif_switch.py
@@ -0,0 +1,33 @@
+print("""Menu: 
+      1. File
+      2. View
+      3. Exit
+      """)
+
+choice = input("Enter your choice: ")
+if choice == "1" :
+    print("You have selected 'File' menu")
+elif choice == "2" :
+    print("You have selected 'View' menu")
+elif choice == "3" :
+    print("You have selected 'Exit' menu")
+else:
+    print("Incorrect choice")
+
+print()
+
+print("""Menu: 
+      1. File
+      2. View
+      3. Exit
+      """)
+
+choice = int(input("Enter your choice: "))
+if choice == 1 :
+    print("You have selected 'File' menu")
+elif choice == 2 :
+    print("You have selected 'View' menu")
+elif choice == 3 :
+    print("You have selected 'Exit' menu")
+else:
+    print("Incorrect choice")
