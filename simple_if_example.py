c6eab596c9ec26eeadf3d5d06b69da316055ea86
Savikov Sergey <mega.savik25@mail.ru>
2022-07-04 15:52:45 +0300

    оператор if

---
+++ simple_if_example.py
@@ -0,0 +1,21 @@
+number = int(input("Enter a number: "))
+if number > 5:
+    print("This number is greater than five")
+
+print()
+
+number = int(input("Enter a number: "))
+if number > 5: print("This number is greater than five")
+
+print()
+
+pass
+
+number = int(input("Enter a number: "))
+if number > 5:
+ print("This number is greater than five")
+
+pass
+
+if number is not None:
+    pass  # TODO: add some logic later
