b6db91b43288f9590f61e707508677a57a4cd661
Savikov Sergey <mega.savik25@mail.ru>
2022-07-05 16:53:52 +0300

    while_else(Lesson 4)

---
+++ while_else.py
@@ -0,0 +1,16 @@
+counter = 5
+while counter != 0:
+    print(counter)
+    counter -= 1
+else:
+    print("The loop is complete")
+    print("counter= ", counter)
+
+print()
+
+counter = 5
+while counter != 0:
+    print(counter)
+    counter -= 1
+print("The loop is complete")
+print("counter= ", counter)
