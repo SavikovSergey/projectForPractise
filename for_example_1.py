6dc0017939390a9ce2e6de502ccd596b08c129d3
Savikov Sergey <mega.savik25@mail.ru>
2022-07-05 17:16:33 +0300

    for_examples(Lesson 4)

---
+++ for_example_1.py
@@ -0,0 +1,18 @@
+for i in range(10):
+    print("i= ", i)
+
+print()
+
+for i in range(5,15):
+    print("i= ", i)
+
+print()
+
+for i in range(5,16,5):
+    print("i= ", i)
+
+print()
+
+for i in range(10,0,-3):
+    print("i= ", i)
+
