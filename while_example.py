f2b7092ec26129e3fa4ca8ced12021675da5f249
Savikov Sergey <mega.savik25@mail.ru>
2022-07-05 15:44:00 +0300

    while_example_1(Lesson 4)

---
+++ while_example.py
@@ -0,0 +1,9 @@
+response = ""
+while response != "exit":
+    response = input("Type 'exit' to exit \n ")
+
+print()
+
+response = "exit"
+while response != "exit":
+    response = input("Type 'exit' to exit \n ")
