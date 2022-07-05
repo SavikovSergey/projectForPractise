e212a736a8091a3fc35d259fa7bef46460b81131
Savikov Sergey <mega.savik25@mail.ru>
2022-07-05 21:33:54 +0300

    Hello_name(Lesson 1)

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello, World")
+
+print()
+
+print("value1", "value2", "value3")
+
+print()
+
+name = "Alex"
+print(name)
+
+print()
+
+name = input("What is your name? ")
+print("Hello,", name)
