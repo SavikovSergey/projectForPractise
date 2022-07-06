987375ac4d5038ab5631fa8d48fc6657efe21401
Savikov Sergey <mega.savik25@mail.ru>
2022-07-06 17:34:10 +0300

    function_example6(Lesson 5)

---
+++ function_example6.py
@@ -0,0 +1,15 @@
+def print_info(object_name, color, price,):
+    print('Object: ', object_name, sep='\t')
+    print('Color: ', color, sep='\t')
+    print('Price', price, sep='\t')
+    print()
+
+print_info("pen", "blue", 1)
+
+print_info(object_name= "pen",
+           color = "blue",
+           price = 1)
+
+print_info(price=5, object_name='cup', color='red')
+
+print_info('coffee',price=10, color='black')
