19697c6e8d8d3f56a917548b268af4286c351c6e
Savikov Sergey <mega.savik25@mail.ru>
2022-07-06 16:49:37 +0300

    function_example_3(Lesson 5)

---
+++ example_3(function).py
@@ -0,0 +1,39 @@
+for _ in range(10):
+    print("Hello")
+
+
+def print_numbers(limit):
+    for i in range(limit):
+         print(i)
+
+print_numbers(10)
+print()
+print_numbers(8)
+
+print()
+
+def print_numbers(limit):
+    for i in range(limit):
+         print(i)
+
+def main():
+    print_numbers(10)
+    print()
+    print_numbers(8)
+
+main()
+
+print()
+
+import EXAMPLE111
+def print_numbers(limit):
+    for i in range(limit):
+         print(i)
+
+def main():
+    EXAMPLE111.hello()
+    print_numbers(10)
+    print()
+    print_numbers(8)
+
+main()
