95c0c06a9255e604a31432bd1986bf14edf92c87
Savikov Sergey <mega.savik25@mail.ru>
2022-07-09 20:50:31 +0300

    lists(Lesson 7)

---
+++ list_definition.py
@@ -0,0 +1,15 @@
+int_list = [1,2,3,5]
+char_list = ['a', 'b', 'c','d']
+empty_list = []
+
+print("Numbers: ", int_list)
+print("Chars: ", char_list)
+print("Empty list: ", empty_list)
+
+print()
+
+list_from_range = list(range(5))
+print("From range: ", list_from_range)
+
+list_from_string = list("a string")
+print("From string: ", list_from_string)
