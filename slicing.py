3326c5f0f6787dd23de335cafb9431768817324a
Savikov Sergey <mega.savik25@mail.ru>
2022-07-09 22:01:38 +0300

    slicing_lists(Lesson 7)

---
+++ slicing.py
@@ -0,0 +1,23 @@
+my_list = [5, 7, 9, 1, 1, 2]
+
+sub_list = my_list[0:3]
+print(sub_list)
+
+list = my_list[2:-1]
+print(list)
+print(my_list[4:5])
+print((my_list[4]))
+
+print()
+
+sub_list = my_list[0::2]
+print(sub_list)
+print(my_list[2:-2:2])
+print(my_list[-1:0:-1])
+print(my_list[-1::-1])
+# print()
+# my_list = [5, 7, 9, 1, 1, 2]
+#
+# sub_list = my_list[:3]
+# print(sub_list)
+
