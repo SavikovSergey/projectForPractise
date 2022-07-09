f75238a07447924d5a463f2154c32ec0ed35ddd6
Savikov Sergey <mega.savik25@mail.ru>
2022-07-09 22:08:35 +0300

    len of list(Lesson 7)

--- len_list.py
+++ len_list.py
@@ -1,7 +1,7 @@
-my_list = [5, 7, 9, 1, 1, 2]
+my_list = [5, 7, 9, 1, 1, 2, 123]
 
-value = int(input('Enter a number: '))
-if value in my_list:
-    print("This number is in list")
-else:
-    print("This number is not in list")
+print("Number of elements: ", len(my_list))
+print()
+my_list = []
+
+print("Number of elements: ", len(my_list))
